edde0f30550b57312652429b8eff449efd0b356b
amar-at-iitm <ma24m002@smail.iitm.ac.in>
2025-09-04 16:37:04 +0530

    improved for  upload_files functionality

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -10,6 +10,8 @@ RATE_LOW=10
 RATE_HIGH=10000
 DURATION_SHORT=5
 DURATION_LONG=2
+PROVIDED_IP="127.0.0.1"    # Replace with PROVIDED IP
+PROVIDED_PORT="5000"        # Replace with PROVIDED PORT
 
 mkdir -p "${RESULTS_DIR}"
 
@@ -34,25 +36,25 @@ echo "[SWARM] Waiting for service to stabilize..."
 sleep 10  # Can be replaced this with a readiness check
 
 echo "[SWARM] Running test (basic curl)..."
-curl -s http://localhost:5000 || echo "[WARN] Swarm service may not have started yet"
+curl -s http://127.0.0.1:5000 || echo "[WARN] Swarm service may not have started yet"
 
 
 
 mkdir -p "${RESULTS_DIR}"
 
 echo "[INFO] Running low-load test (rate=10, duration=${DURATION_SHORT}s)..."
-python3 client/client.py --target "http://localhost:5000" --rate ${RATE_LOW} \
+python3 client/client.py --target "http://127.0.0.1:5000" --rate ${RATE_LOW} \
   --duration ${DURATION_SHORT} \
   --output "${RESULTS_DIR}/docker_response_${RATE_LOW}" \
   --mode sync
      
 
 echo "[INFO] Running high-load test (rate=10000, duration=${DURATION_LONG}s)..."
-python3 client/client.py --target "http://localhost:5000" --rate ${RATE_HIGH} \
+python3 client/client.py --target "http://127.0.0.1:5000" --rate ${RATE_HIGH} \
   --output "${RESULTS_DIR}/docker_response_${RATE_HIGH}" \
   --duration ${DURATION_LONG} \
-  --mode sync
-
+  --mode sync \
+  --upload_url http://${PROVIDED_IP}:${PROVIDED_PORT}/
 
 echo "[INFO] Docker Swarm tests done. Results saved in ./${RESULTS_DIR}/"
 
@@ -105,12 +107,14 @@ mkdir -p "${RESULTS_DIR}"
 echo "[INFO] Running low-load test (rate=${RATE_LOW}, duration=${DURATION_SHORT}s)..."
 python3 client/client.py --target "${K8S_URL}" --rate ${RATE_LOW} \
   --output "${RESULTS_DIR}/kubernetes_response_${RATE_LOW}" \
-  --duration ${DURATION_SHORT}
+  --duration ${DURATION_SHORT} \
+  --upload_url http://${PROVIDED_IP}:${PROVIDED_PORT}/
 
 echo "[INFO] Running high-load test (rate=${RATE_HIGH}, duration=${DURATION_LONG}s)..."
 python3 client/client.py --target "${K8S_URL}" --rate ${RATE_HIGH} \
   --output "${RESULTS_DIR}/kubernetes_response_${RATE_HIGH}" \
-  --duration ${DURATION_LONG}
+  --duration ${DURATION_LONG} \
+  --upload_url http://${PROVIDED_IP}:${PROVIDED_PORT}/
 
 echo "[INFO] All Kubernetes tests done. Results saved in ./${RESULTS_DIR}/"
 
